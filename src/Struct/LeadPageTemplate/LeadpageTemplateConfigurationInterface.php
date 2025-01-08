5d2f0072411c0f70163ab690c4720a923d5111dd
mbruckmoser <bruckmoser@sc-networks.com>
2025-01-08 16:24:32 +0100

    Add LeadPagetemplate config

---
+++ src/Struct/LeadPageTemplate/LeadpageTemplateConfigurationInterface.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace Scn\EvalancheSoapStruct\Struct\LeadPageTemplate;
+
+use Scn\EvalancheSoapStruct\Struct\LeadPage\LeadpageConfiguration;
+use Scn\EvalancheSoapStruct\Struct\StructInterface;
+
+interface LeadpageTemplateConfigurationInterface extends StructInterface
+{
+    public function getMacroLibrary(): ?string;
+
+    public function setMacroLibrary(string $macroLibrary): LeadpageTemplateConfiguration;
+
+    public function getInputfield0(): ?string;
+
+    public function setInputfield0(string $inputfield0): LeadpageTemplateConfiguration;
+
+    public function getInputfield1(): ?string;
+
+    public function setInputfield1(string $inputfield1): LeadpageTemplateConfiguration;
+
+    public function getInputfield2(): ?string;
+
+    public function setInputfield2(string $inputfield2): LeadpageTemplateConfiguration;
+
+    public function getInputfield3(): ?string;
+
+    public function setInputfield3(string $inputfield3): LeadpageTemplateConfiguration;
+
+    public function getInputfield4(): ?string;
+
+    public function setInputfield4(string $inputfield4): LeadpageTemplateConfiguration;
+
+    public function getInputfield5(): ?string;
+
+    public function setInputfield5(string $inputfield5): LeadpageTemplateConfiguration;
+
+    public function getInputfield6(): ?string;
+
+    public function setInputfield6(string $inputfield6): LeadpageTemplateConfiguration;
+
+    public function getInputfield7(): ?string;
+
+    public function setInputfield7(string $inputfield7): LeadpageTemplateConfiguration;
+
+    public function getInputfield8(): ?string;
+
+    public function setInputfield8(string $inputfield8): LeadpageTemplateConfiguration;
+
+    public function getInputfield9(): ?string;
+
+    public function setInputfield9(string $inputfield9): LeadpageTemplateConfiguration;
+
+    public function getTextarea0(): ?string;
+
+    public function setTextarea0(string $textarea0): LeadpageTemplateConfiguration;
+
+    public function getTextarea1(): ?string;
+
+    public function setTextarea1(string $textarea1): LeadpageTemplateConfiguration;
+
+    public function getTextarea2(): ?string;
+
+    public function setTextarea2(string $textarea2): LeadpageTemplateConfiguration;
+
+    public function getTextarea3(): ?string;
+
+    public function setTextarea3(string $textarea3): LeadpageTemplateConfiguration;
+
+    public function getTextarea4(): ?string;
+
+    public function setTextarea4(string $textarea4): LeadpageTemplateConfiguration;
+
+    public function getTextarea5(): ?string;
+
+    public function setTextarea5(string $textarea5): LeadpageTemplateConfiguration;
+
+    public function getTextarea6(): ?string;
+
+    public function setTextarea6(string $textarea6): LeadpageTemplateConfiguration;
+
+    public function getTextarea7(): ?string;
+
+    public function setTextarea7(string $textarea7): LeadpageTemplateConfiguration;
+
+    public function getTextarea8(): ?string;
+
+    public function setTextarea8(string $textarea8): LeadpageTemplateConfiguration;
+
+    public function getTextarea9(): ?string;
+
+    public function setTextarea9(string $textarea9): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea0(): ?string;
+
+    public function setHtmlarea0(string $htmlarea0): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea1(): ?string;
+
+    public function setHtmlarea1(string $htmlarea1): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea2(): ?string;
+
+    public function setHtmlarea2(string $htmlarea2): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea3(): ?string;
+
+    public function setHtmlarea3(string $htmlarea3): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea4(): ?string;
+
+    public function setHtmlarea4(string $htmlarea4): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea5(): ?string;
+
+    public function setHtmlarea5(string $htmlarea5): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea6(): ?string;
+
+    public function setHtmlarea6(string $htmlarea6): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea7(): ?string;
+
+    public function setHtmlarea7(string $htmlarea7): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea8(): ?string;
+
+    public function setHtmlarea8(string $htmlarea8): LeadpageTemplateConfiguration;
+
+    public function getHtmlarea9(): ?string;
+
+    public function setHtmlarea9(string $htmlarea9): LeadpageTemplateConfiguration;
+}
